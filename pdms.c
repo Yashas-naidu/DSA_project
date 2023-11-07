fb2bc39fdf59fc08321441beb7a48393d0ae3e33
Yashas HG <yashashsn555@gmail.com>
2023-11-07 09:48:06 +0530

    hashing used

--- pdms.c
+++ pdms.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <time.h>
 
+#define TABLE_SIZE 1000
+
 struct Patient {
     int id;
     char name[100];
@@ -11,6 +13,10 @@ struct Patient {
     char address[100];
     char phone[15];
     struct Patient* next;
+};
+
+struct HashTable {
+    struct Patient* table[TABLE_SIZE];
 };
 
 struct Patient* createPatient(char name[], int age, char gender[], char address[], char phone[]) {
@@ -25,40 +31,48 @@ struct Patient* createPatient(char name[], int age, char gender[], char address[
     return newPatient;
 }
 
-void savePatientsToFile(struct Patient* head);
+void savePatientsToFile(struct HashTable* hashTable);
 
-void addPatient(struct Patient** head, struct Patient* newPatient) {
-    if (*head == NULL) {
-        *head = newPatient;
+int hashFunction(int id) {
+    return id % TABLE_SIZE;
+}
+
+void addPatient(struct HashTable* hashTable, struct Patient* newPatient) {
+    int index = hashFunction(newPatient->id);
+    if (hashTable->table[index] == NULL) {
+        hashTable->table[index] = newPatient;
     } else {
-        struct Patient* current = *head;
+        struct Patient* current = hashTable->table[index];
         while (current->next != NULL) {
             current = current->next;
         }
         current->next = newPatient;
     }
     printf("Patient added successfully. ID: %03d\n", newPatient->id);
-    savePatientsToFile(*head);
+    savePatientsToFile(hashTable);
 }
 
-void savePatientsToFile(struct Patient* head) {
+void savePatientsToFile(struct HashTable* hashTable) {
     FILE* file = fopen("patients.txt", "w");
     if (file == NULL) {
         printf("Error opening file.\n");
         return;
     }
 
-    struct Patient* current = head;
-    while (current != NULL) {
-        fprintf(file, "%03d|%s|%d|%s|%s|%s\n", current->id, current->name, current->age, current->gender, current->address, current->phone);
-        current = current->next;
+    int i;
+    for (i = 0; i < TABLE_SIZE; i++) {
+        struct Patient* current = hashTable->table[i];
+        while (current != NULL) {
+            fprintf(file, "%03d|%s|%d|%s|%s|%s\n", current->id, current->name, current->age, current->gender, current->address, current->phone);
+            current = current->next;
+        }
     }
     
     fclose(file);
     printf("Patients data written to file.\n");
 }
 
-void readPatientsFromFile(struct Patient** head) {
+void readPatientsFromFile(struct HashTable* hashTable) {
     FILE* file = fopen("patients.txt", "r");
     if (file == NULL) {
         printf("No existing patient data found.\n");
@@ -71,10 +85,11 @@ void readPatientsFromFile(struct Patient** head) {
         sscanf(line, "%d|%[^|]|%d|%[^|]|%[^|]|%s\n", &newPatient->id, newPatient->name, &newPatient->age, newPatient->gender, newPatient->address, newPatient->phone);
         newPatient->next = NULL;
 
-        if (*head == NULL) {
-            *head = newPatient;
+        int index = hashFunction(newPatient->id);
+        if (hashTable->table[index] == NULL) {
+            hashTable->table[index] = newPatient;
         } else {
-            struct Patient* current = *head;
+            struct Patient* current = hashTable->table[index];
             while (current->next != NULL) {
                 current = current->next;
             }
@@ -86,69 +101,65 @@ void readPatientsFromFile(struct Patient** head) {
     printf("Patients data read from file.\n");
 }
 
-void searchPatient(struct Patient* head, int id) {
-    if (head == NULL) {
-        printf("No patients in the database.\n");
-    } else {
-        struct Patient* current = head;
-        int found = 0;
-        while (current != NULL) {
-            if (current->id == id) {
-                printf("Patient found:\n");
-                printf("ID: %03d\n", current->id);
-                printf("Name: %s\n", current->name);
-                printf("Age: %d\n", current->age);
-                printf("Gender: %s\n", current->gender);
-                printf("Address: %s\n", current->address);
-                printf("Phone: %s\n", current->phone);
-                found = 1;
-                break;
-            }
-            current = current->next;
+void searchPatient(struct HashTable* hashTable, int id) {
+    int index = hashFunction(id);
+    struct Patient* current = hashTable->table[index];
+    int found = 0;
+    while (current != NULL) {
+        if (current->id == id) {
+            printf("Patient found:\n");
+            printf("ID: %03d\n", current->id);
+            printf("Name: %s\n", current->name);
+            printf("Age: %d\n", current->age);
+            printf("Gender: %s\n", current->gender);
+            printf("Address: %s\n", current->address);
+            printf("Phone: %s\n", current->phone);
+            found = 1;
+            break;
         }
-        if (!found) {
-            printf("Patient not found.\n");
-        }
+        current = current->next;
+    }
+    if (!found) {
+        printf("Patient not found.\n");
     }
 }
 
-void deletePatient(struct Patient** head, int id) {
-    if (*head == NULL) {
-        printf("No patients in the database.\n");
-    } else {
-        struct Patient* current = *head;
-        struct Patient* prev = NULL;
-        int found = 0;
-        while (current != NULL) {
-            if (current->id == id) {
-                if (prev == NULL) {
-                    *head = current->next;
-                } else {
-                    prev->next = current->next;
-                }
-                free(current);
-                printf("Patient deleted successfully.\n");
-                found = 1;
-                break;
+void deletePatient(struct HashTable* hashTable, int id) {
+    int index = hashFunction(id);
+    struct Patient* current = hashTable->table[index];
+    struct Patient* prev = NULL;
+    int found = 0;
+    while (current != NULL) {
+        if (current->id == id) {
+            if (prev == NULL) {
+                hashTable->table[index] = current->next;
+            } else {
+                prev->next = current->next;
             }
-            prev = current;
-            current = current->next;
+            free(current);
+            printf("Patient deleted successfully.\n");
+            found = 1;
+            break;
         }
-        if (!found) {
-            printf("Patient not found.\n");
-        } else {
-            savePatientsToFile(*head); // Update the file after deleting the patient
-        }
+        prev = current;
+        current = current->next;
+    }
+    if (!found) {
+        printf("Patient not found.\n");
+    } else {
+        savePatientsToFile(hashTable);
     }
 }
 
 int main() {
-    struct Patient* head = NULL;
+    struct HashTable hashTable;
     int choice;
     
     srand(time(NULL)); // Initialize random number generator
     
-    readPatientsFromFile(&head); // Read existing patient data from file
+    memset(hashTable.table, 0, sizeof(hashTable.table)); // Initialize hash table
+    
+    readPatientsFromFile(&hashTable); // Read existing patient data from file
     
     do {
         printf("Patient Database Management\n");
@@ -180,21 +191,21 @@ int main() {
                 scanf("%s", phone);
                 
                 struct Patient* newPatient = createPatient(name, age, gender, address, phone);
-                addPatient(&head, newPatient);
+                addPatient(&hashTable, newPatient);
                 break;
             }
             case 2: {
                 int id;
                 printf("Enter the ID of the patient to search: ");
                 scanf("%d", &id);
-                searchPatient(head, id);
+                searchPatient(&hashTable, id);
                 break;
             }
             case 3: {
                 int id;
                 printf("Enter the ID of the patient to delete: ");
                 scanf("%d", &id);
-                deletePatient(&head, id);
+                deletePatient(&hashTable, id);
                 break;
             }
             case 4: {
@@ -207,8 +218,6 @@ int main() {
             }
         }
     } while (choice != 4);
-    
-    savePatientsToFile(head); // Save patient data to file before exiting
     
     return 0;
 }
